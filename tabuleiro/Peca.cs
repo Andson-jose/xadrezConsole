a423861575c77594c3a3b7e5772373309b5fda86
Andson José <tarsomeupatrao@gmail.com>
2021-08-14 04:47:17 -0300

    Adicionar arquivos de projeto.

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,53 @@
+﻿
+namespace tabuleiro
+{
+   abstract class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int quantidadesDeMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor  )
+        {
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.quantidadesDeMovimentos = 0;
+        }
+        public void incrementarQteMovimentos()
+        {
+            quantidadesDeMovimentos++;
+        }
+        public void decrementarQteMovimentos()
+        {
+            quantidadesDeMovimentos--;
+        }
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i=0; i<tab.linhas; i++)
+            {
+                for (int j=0; j<tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna]; 
+        }
+        public abstract bool[,] movimentosPossiveis();
+        
+
+        
+        
+         
+        
+    }
+}
